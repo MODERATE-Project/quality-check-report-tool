0b497b89a9a33ff1a03f74b813d8df366c3e6f3b
Sergio Flórez <sergio.florez@fundacionctic.org>
2025-03-24 15:47:54 +0100

    fix: report generation redirection

--- src/front/src/constants.js
+++ src/front/src/constants.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'api'
 
 const RULES_SERVICE_URL = `${API_BASE_URL}/upload`
 const REPORT_SERVICE_URL = `${API_BASE_URL}/report`

67545e0f83a9c06483ff2c4a72e77189cbf15a11
Sergio Flórez <sergio.florez@fundacionctic.org>
2025-03-24 13:47:34 +0100

    fix: back uri on preduction

--- src/front/src/constants.js
+++ src/front/src/constants.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5501'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
 const RULES_SERVICE_URL = `${API_BASE_URL}/upload`
 const REPORT_SERVICE_URL = `${API_BASE_URL}/report`
